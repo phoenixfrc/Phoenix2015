dd9ebc8e59b2b2f46c0e4c02378f299f9dceca99
software team2342 <software@team2342.org>
2015-01-08 20:36:56 -0500

    first commit with mechanum drive

---
+++ src/Robot.cpp
@@ -0,0 +1,52 @@
+#include "WPILib.h"
+
+/**
+ * This is a demo program showing how to use Mecanum control with the RobotDrive class.
+ */
+class Robot: public SampleRobot
+{
+
+    // Channels for the wheels
+    const static int frontLeftChannel	= 3;
+    const static int rearLeftChannel	= 0;
+    const static int frontRightChannel	= 1;
+    const static int rearRightChannel	= 2;
+
+    const static int joystickChannel1	= 0;
+    const static int joystickChannel2	= 1;
+
+	RobotDrive robotDrive;	// robot drive system
+	Joystick stick1;			// only joystick
+	Joystick stick2;
+public:
+	Robot() :
+			robotDrive(frontLeftChannel, rearLeftChannel,
+					   frontRightChannel, rearRightChannel),	// these must be initialized in the same order
+			stick1(joystickChannel1),
+			stick2(joystickChannel2)
+// as they are declared above.
+	{
+		robotDrive.SetExpiration(0.1);
+		robotDrive.SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);	// invert the left side motors
+		robotDrive.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);	// you may need to change or remove this to match your robot
+	}
+
+	/**
+	 * Runs the motors with Mecanum drive.
+	 */
+	void OperatorControl()
+	{
+		robotDrive.SetSafetyEnabled(false);
+		while (IsOperatorControl() && IsEnabled())
+		{
+        	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
+        	// This sample does not use field-oriented drive, so the gyro input is set to zero.
+			robotDrive.MecanumDrive_Cartesian(stick1.GetX(), stick1.GetY(), stick2.GetX());
+
+			Wait(0.005); // wait 5ms to avoid hogging CPU cycles
+		}
+	}
+
+};
+
+START_ROBOT_CLASS(Robot);
