f5f35d96c9d07236fdaf0e28179eca265afe2469
Colin <colin.bewley@asdnh.org>
2015-01-10 12:04:14 -0500

    Added Elevator.cpp and Elevator.h

---
+++ src/Elevator.cpp
@@ -0,0 +1,14 @@
+/*
+ * Elevator.cpp
+ *
+ *  Created on: Jan 10, 2015
+ *      Author: Colin
+ */
+
+#include "Elevator.h"
+
+Elevator::Elevator()
+{
+
+};
+
---
+++ src/Elevator.h
@@ -0,0 +1,20 @@
+/*
+ * Elevator.h
+ *
+ *  Created on: Jan 10, 2015
+ *      Author: Colin
+ */
+#ifndef SRC_ELEVATOR_H_
+#define SRC_ELEVATOR_H_
+#include "WPILib.h"
+
+class Elevator{
+	public:
+		Elevator();
+};
+
+
+
+
+
+#endif /* SRC_ELEVATOR_H_ */
