42da76321534551780937b386c21584264c42f0c
team2342 <software@team2342.org>
2015-01-13 20:51:37 -0500

    Added Homing Function and joystick and button control states for elevator.

--- src/Elevator.cpp
+++ src/Elevator.cpp
@@ -6,9 +6,78 @@
  */
 #include "Elevator.h"
 
-Elevator::Elevator()
+Elevator::Elevator():
+	lowerLimit(1),
+	upperLimit(2),
+	home(3),
+	motor1(4),
+	motor2(5)
 {
 
+	currentMode = buttonMode;
+
+
 }
+
+void Elevator::find_home(){
+
+	while (!isHome){
+
+		if (!lowerLimitTriped)
+			{
+
+			motor1.Set(-motorSpeed);		//go down
+			motor2.Set(-motorSpeed);
+
+			}
+		else if (lowerLimitTriped)
+			{
+
+			motor1.Set(motorSpeed);			//go up
+			motor2.Set(motorSpeed);
+
+			}
+	}
+	int encoder_value = 0;
+}
+
+
+
+
+
+void Elevator::operateElevator(){
+
+	if (upperLimitTriped){
+
+		motor1.Set(0);
+		motor2.Set(0);
+		}
+	else if (lowerLimitTriped){
+		motor1.Set(0);
+		motor2.Set(0);
+		}
+
+	switch (currentMode){
+
+	case buttonMode:
+		break;
+
+	case joystickMode:
+		break;
+
+
+	}
+
+
+
+
+}
+
+
+
+
+
+
+
 
 Elevator::~Elevator(){}
--- src/Elevator.h
+++ src/Elevator.h
@@ -4,14 +4,35 @@
  *  Created on: Jan 10, 2015
  *      Author: Developer
  */
-
+#include "WPILib.h"
 #ifndef SRC_ELEVATOR_H_
 #define SRC_ELEVATOR_H_
 class Elevator {
+
+	DigitalInput lowerLimit;
+	DigitalInput upperLimit;
+	DigitalInput home;
+	Talon motor1;
+	Talon motor2;
+
+	enum elevatorStates{joystickMode, buttonMode};
+
+	bool isHome = home.Get();
+	bool upperLimitTriped = upperLimit.Get();
+	bool lowerLimitTriped = lowerLimit.Get();
+	float encoder_value=0;
+	float motorSpeed = .5;
+	elevatorStates currentMode;
+
+
 public:
 	Elevator();
+	void find_home();
+	void operateElevator();
 	~Elevator();
+
 };
+
 
 
 
