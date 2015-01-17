5b66a5b7924349ba83632fc1a20167ff85b1e121
team machine <software@team2342.org>
2015-01-17 13:12:18 -0500

    This has the correct port assignments

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -8,10 +8,10 @@ class Robot: public SampleRobot
 {
 
     // Channels for the wheels
-    const static int frontLeftChannel	= 3;
+    const static int frontLeftChannel	= 1;
     const static int rearLeftChannel	= 0;
-    const static int frontRightChannel	= 1;
-    const static int rearRightChannel	= 2;
+    const static int frontRightChannel	= 2;
+    const static int rearRightChannel	= 3;
 
     const static int joystickChannel	= 0;
     const static int testEncoderChannelA = 1;
