b7c3b3cf98f8472bdcb79642c5b4134541de081e
team2342 <software@team2342.org>
2015-01-18 16:02:54 -0500

    Added Team2342 Joystick Class

--- src/Team2342Joystick.h
+++ src/Team2342Joystick.h
@@ -1,15 +1,17 @@
 /*
- * DeadZoneJoystick.h
+ * DeadZoneJoystick.h'
  *
  *  Created on: Jan 17, 2015
  *      Author: Developer
  */
 
+#include "WPILib.h"
+
 #ifndef SRC_TEAM2342JOYSTICK_H_
 #define SRC_TEAM2342JOYSTICK_H_
 
-class Team2342Joystick: public Joystick{ //This class is our customization of 'class Joystick'.
-
+class Team2342Joystick: public Joystick { //This class is our customization of 'class Joystick'.
+	float GetZWithDeadZone(float deadZone);
 };
 
 
