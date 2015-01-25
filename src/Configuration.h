3353b5a5f17d2a10d7648f1aa4af3804a152a0bc
Benjamin Nichols-Farquhar <erogone775@gmail.com>
2015-01-25 16:27:38 -0500

    Set up Configuration.h

---
+++ src/Configuration.h
@@ -0,0 +1,27 @@
+#include "WPILib.h"
+
+#ifndef SRC_CONFIGURATION_H_
+#define SRC_CONFIGURATION_H_
+
+
+
+class Configuration {
+
+
+
+
+
+
+
+
+
+public:
+	Configuration();
+
+	void ConfigurationInit();
+
+	~Configuration();
+};
+
+
+#endif /* SRC_CONFIGURATION_H_ */
