be59e6edd47efc9313a7ecf0772ed689166246cf
Kurtis Duggan <kurtis.duggan@team2342.org>
2015-01-30 19:03:29 -0500

    Removed the MoveDistance function because the new code Ray and I wrote makes it obsolete

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -103,36 +103,6 @@ public:
 		m_rightFrontDriveEncoder.SetReverseDirection(true);
 		SmartDashboard::init();
 	}
-	//Move a given distance (in inches). This is a very early concept of it and will probably change when we implement encoders
-	void MoveDistance(float xDistance, float yDistance)
-	{
-	    //Assuming that 1 is the max input for a joystick because if I assume it's 255 and it turns out to be 1 the robot might have issues while testing
-	    //This will move the robot in an L shape for now (x first)
-	    if (xDistance != 0)
-	    {
-	        if (xDistance > 0)
-	        {
-	            robotDrive.MecanumDrive_Cartesian(1,0,0);
-	        }
-	        else
-	        {
-	            robotDrive.MecanumDrive_Cartesian(-1,0,0);
-	        }
-	        Wait(xDistance/4);
-	    }
-	    if (yDistance != 0)
-	    {
-            if (yDistance > 0)
-            {
-                robotDrive.MecanumDrive_Cartesian(0,1,0);
-            }
-            else
-            {
-                robotDrive.MecanumDrive_Cartesian(0,-1,0);
-            }
-	        Wait(yDistance/4);
-	    }
-	}
 	void Autonomous()
 	{
 	    switch(autoMode)
