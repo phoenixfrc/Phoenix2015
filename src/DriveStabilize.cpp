a496cf8d0263ecc8eb254ff9623849497f16ba9b
Ray Lucas <Ray.Lucas@team2342.org>
2015-02-07 14:43:11 -0500

    Created DriveStabilize stubs.

---
+++ src/DriveStabilize.cpp
@@ -0,0 +1,14 @@
+/*
+ * DriveStabilize.cpp
+ *
+ *  Created on: Feb 7, 2015
+ *      Author: Ray
+ */
+
+#include "WPILib.h"
+#include "DriveStabilize.h"
+
+
+DriveStabilize::DriveStabilize(){
+
+}
---
+++ src/DriveStabilize.h
@@ -0,0 +1,20 @@
+/*
+ * driveStabilize.h
+ *
+ *  Created on: Feb 3, 2015
+ *      Author: Ray
+ */
+
+#include "WPILib.h"
+
+#ifndef SRC_DRIVESTABILIZE_H_
+#define SRC_DRIVESTABILIZE_H_
+
+
+class DriveStabilize {
+public:
+    DriveStabilize();
+};
+
+
+#endif /* SRC_DRIVESTABILIZE_H_ */
