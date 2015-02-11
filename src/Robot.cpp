48c25e1ca46212baaf2bdfdb711691c1dce5de8f
John P <software@team2342.org>
2015-02-10 21:19:41 -0500

    Changed DIO 18 and 19 to 14 and 15. Put draggerLowerLimit and draggerUpperLimit in their place.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -34,12 +34,13 @@ class Robot: public SampleRobot
     DigitalInput m_elevatorUpperLimit;
     DigitalInput m_elevatorHomeSwitch;
     DigitalInput m_opticalSensor;
+    //MXP Breakout board doesn't expose ports: 14-17
+//    DigitalInput m_DIO14;
+//    DigitalInput m_DIO15;
+//    DigitalInput m_DIO16;
+//    DigitalInput m_DIO17;
     DigitalInput m_draggerLowerLimit;
     DigitalInput m_draggerUpperLimit;
-    DigitalInput m_DIO16;
-    DigitalInput m_DIO17;
-    DigitalInput m_DIO18;
-    DigitalInput m_DIO19;
     DigitalInput m_DIO20;
     DigitalInput m_DIO21;
     DigitalInput m_DIO22;
@@ -80,12 +81,12 @@ public:
         m_elevatorHomeSwitch(PortAssign::ElevatorHomeSwitchChannel),
 
         m_opticalSensor(PortAssign::OpticalSensorChannel),
-        m_draggerLowerLimit(PortAssign::DraggerLowerLimitChannel),
-        m_draggerUpperLimit(PortAssign::DraggerUpperLimitChannel),
-        m_DIO16(PortAssign::DIO16Channel),
-        m_DIO17(PortAssign::DIO17Channel),
-        m_DIO18(PortAssign::DIO18Channel),
-        m_DIO19(PortAssign::DIO19Channel),
+        //m_DIO14(PortAssign::DIO14Channel),
+        //m_DIO15(PortAssign::DIO15Channel),
+        //m_DIO16(PortAssign::DIO16Channel),
+        //m_DIO17(PortAssign::DIO17Channel),
+        m_draggerLowerLimit(PortAssign::DraggerActivatedLimitChannel),
+        m_draggerUpperLimit(PortAssign::DraggerReservedLimitChannel),
         m_DIO20(PortAssign::DIO20Channel),
         m_DIO21(PortAssign::DIO21Channel),
         m_DIO22(PortAssign::DIO22Channel),
@@ -194,12 +195,13 @@ public:
                 m_elevatorUpperLimit.Get() <<
                 m_elevatorHomeSwitch.Get() <<
                 m_opticalSensor.Get() <<
+                "-" <<
+                //m_DIO14.Get() <<
+                //m_DIO15.Get() <<
+                //m_DIO16.Get() <<
+                //m_DIO17.Get() <<
                 m_draggerLowerLimit.Get() <<
                 m_draggerUpperLimit.Get() <<
-                m_DIO16.Get() <<
-                m_DIO17.Get() <<
-                m_DIO18.Get() <<
-                m_DIO19.Get() <<
                 m_DIO20.Get() <<
                 m_DIO21.Get() <<
                 m_DIO22.Get() <<
