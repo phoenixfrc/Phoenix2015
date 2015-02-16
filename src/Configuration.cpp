a45723d3bef60dd681d6dd253ec5399759bbb288
John P <software@team2342.org>
2015-02-16 18:39:35 -0500

    try to debug prefs.  Save seems to not work if using structure elements

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -25,9 +25,11 @@ struct ourConfiguration_t {
  *
  * The fourth column is the default value (if not found in the roborio config upon starting)
  */
-struct ourConfiguration_t ourConfiguration [] =
+static struct ourConfiguration_t ourConfiguration [] =
 {
         {"b", "this is b", configFloat, "12.34"},
+        {"second", "this is second float", configFloat, "222.34"},
+        {"third", "this is third", configFloat, "333.34"},
         {"bar", "this is bar", configString, "this is string bar"},
         //Add new entries above here
         {"0", "0", endOfList, "0"}//Keep this here ALWAYS!
@@ -51,11 +53,8 @@ void Configuration::configurationInit()
     }
     _configInstance = new Configuration();
     _configInstance->ReadValues();
-    fflush(stdout);
-    Wait(0.25);
     _configInstance->Save();
     Wait(0.25);
-    fflush(stdout);
 }
 
 /*
@@ -87,8 +86,19 @@ void Configuration::ReadValues()
         printf("%s\n", ourConfiguration[currentConfigEntry].humanName);
         if(ourConfiguration[currentConfigEntry].type == configFloat)
         {
+             printf("have key %s\n", ourConfiguration[currentConfigEntry].key);
+             float valueToSave = std::stof(ourConfiguration[currentConfigEntry].defaultValue);
+             printf("value for float key %s is %f\n", ourConfiguration[currentConfigEntry].key, valueToSave);
       //      PutFloat("f", 15.23F);
-              PutFloat(ourConfiguration[currentConfigEntry].key, 15.23F);
+             _configInstance->PutFloat(ourConfiguration[currentConfigEntry].key, valueToSave);
+             if (_configInstance->ContainsKey("second")) {
+                 printf ("checked for second key and it's there\n");
+             }
+             else
+             {
+                 printf ("checked for second key and it's NOT THERE\n");
+
+             }
 
            // printf("Found a float %s\n", ourConfiguration[currentConfigEntry].humanName);
            // float valueToSave = 123.456;// std::stof("123.456");
@@ -109,6 +119,14 @@ void Configuration::ReadValues()
    // PutFloat("f", 15.53F);
     PutInt("d", 555);
     PutString("e", "this is string e");
+    PutString("eas", "this is string e");
+    PutString("edf", "this is string e");
+    PutString("egs", "this is string e");
+    PutString("egt", "this is string e");
+    // const char * keyforFFFF = ourConfiguration[1].key;
+    const char * keyforFFFF = "ffff";
+    float ffff = 555.1234f;
+    _configInstance->PutFloat(keyforFFFF, ffff);
     fflush(stdout);
 }
 
