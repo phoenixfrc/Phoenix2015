9e6e2bca87a883acecb509efc4d2b66c2d0d68a2
Brin <Brin@192.168.80.153>
2015-03-02 20:29:21 -0500

    Created ElevatorBackup class and a corresponding branch, ElevatorBackupTesting

---
+++ ElevatorBackup.cpp
@@ -0,0 +1,10 @@
+/*
+ * ElevatorBackup.cpp
+ *
+ *  Created on: Mar 2, 2015
+ *      Author: Brin
+ */
+#include "ElevatorBackup.h"
+
+
+
---
+++ ElevatorBackup.h
@@ -0,0 +1,15 @@
+/*
+ * ElevatorBackup.h
+ *
+ *  Created on: Mar 2, 2015
+ *      Author: Brin
+ */
+
+#ifndef ELEVATORBACKUP_H_
+#define ELEVATORBACKUP_H_
+
+
+
+
+
+#endif /* ELEVATORBACKUP_H_ */
