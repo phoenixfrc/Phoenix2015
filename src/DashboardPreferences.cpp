71d4139d72263bf639c1764a7b265071fda495cf
team2342 <software@team2342.org>
2015-03-02 21:05:34 -0500

    Stub Class for DashboardPreferences

---
+++ src/DashboardPreferences.cpp
@@ -0,0 +1,18 @@
+/*
+ * DashboardPreferences.cpp
+ *
+ *  Created on: Mar 2, 2015
+ *      Author: Developer
+ */
+#include "DashboardPreferences.h"
+
+DashboardPreferences::DashboardPreferences()
+{}
+
+void DashboardPreferences::customPreferences(){
+
+}
+
+DashboardPreferences::~DashboardPreferences(){
+
+}
---
+++ src/DashboardPreferences.h
@@ -0,0 +1,28 @@
+/*
+ * DashboardPreferences.h
+ *
+ *  Created on: Mar 2, 2015
+ *      Author: Developer
+ */
+
+#ifndef SRC_DASHBOARDPREFERENCES_H_
+#define SRC_DASHBOARDPREFERENCES_H_
+#include "WPILib.h"
+#include "Constants.h"
+#include <sstream>
+
+class DashboardPreferences{
+public:
+	DashboardPreferences();
+
+	void customPreferences();
+
+	~DashboardPreferences();
+
+private:
+
+};
+
+
+
+#endif /* SRC_DASHBOARDPREFERENCES_H_ */
