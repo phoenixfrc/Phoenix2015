aa2a0d282d03614b824b62e64edec298e9fb077a
team2342 <software@team2342.org>
2015-03-02 21:19:41 -0500

    Started to get dashboard button values.

--- src/DashboardPreferences.cpp
+++ src/DashboardPreferences.cpp
@@ -10,6 +10,9 @@ DashboardPreferences::DashboardPreferences()
 {}
 
 void DashboardPreferences::customPreferences(){
+	bool m_button1 = SmartDashboard::GetBoolean("DB/Button 1", true);
+	bool m_button2 = SmartDashboard::GetBoolean("DB/Button 2", false);
+	bool m_button3 = SmartDashboard::GetBoolean("DB/Button 3", false);
 
 }
 
