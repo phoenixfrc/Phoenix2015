a6bed67d2d960ac761027936f5c359a08c05c5ba
Benjamin Nichols-Farquhar <erogone775@gmail.com>
2015-03-08 17:34:51 -0400

    Tuned Complex Auto

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -117,12 +117,13 @@ public:
         m_DIO24(PortAssign::DIO24Channel),
         m_DIO25(PortAssign::DIO25Channel),
 
-        m_gyro(PortAssign::GyroChannel),
-
         m_IRLeftInner(PortAssign::IRLeftInnerChannel),
+		m_IRLeftOuter(PortAssign::IRLeftOuterChannel),
         m_IRRightInner(PortAssign::IRRightInnerChannel),
-        m_IRLeftOuter(PortAssign::IRLeftOuterChannel),
-        m_IRRightOuter(PortAssign::IRRightOuterChannel),
+		m_IRRightOuter(PortAssign::IRRightOuterChannel),
+
+		m_gyro(PortAssign::GyroChannel),
+
 
         m_stick(PortAssign::JoystickChannel),
         m_gamepad(PortAssign::GamepadChannel),
@@ -217,7 +218,7 @@ public:
                                 "Lift 2 Back");
             MoveAndLiftWithDelay((-FieldDistances::autoCrateDiff - FieldDistances::shiftDiff), 0, 1, kElevatorHook3Ready, -49,
                     "1,2 Left and Down");
-            Move(0, FieldDistances::intoAutoDiff -FieldDistances::moveBack, 1,
+            MoveAndLift(0, FieldDistances::intoAutoDiff -FieldDistances::moveBack,  1, kElevatorHook2Lifted,
                     "Into Autozone");
             Lift(kSoftLowerLimit,
                     "Put down all");
